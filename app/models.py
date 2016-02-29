3fc06e8d959423bc44a0d1ab79bb327b54a537d1
Alan Chen <alanchen@nyu.edu>
2016-02-28 22:07:13 -0500

    datetime import

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@
 #
 
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,9 +24,10 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     title = db.Column(db.String(255))
     description = db.Column(db.Text)
-    date_posted = db.Column(db.DateTime, default=datetime.now()
+    date_posted = db.Column(db.DateTime, default = datetime.now())
     status = db.Column(db.Enum('success','pending','failed'))
