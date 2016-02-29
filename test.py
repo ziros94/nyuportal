331ff1b241025643b2f6a85184953364e69d72d6
Alan Chen <alanchen@nyu.edu>
2016-02-28 22:34:08 -0500

    test case for user

--- test.py
+++ test.py
@@ -7,5 +7,13 @@ db.create_all()
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
+print "Adding New User"
+user = User("alchen", "password", "a@nyu.edu", "alan", "chen")
+db.session.add(user)
+
+db.session.commit()
+
+print "User Title: " + User.query.filter_by(username='alchen').first().title
+
 print Post.query.all()
 print User.query.all()
