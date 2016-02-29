26076a454707bab25605aa73037f5ced5bb5b843
ziros94 <alvi.kabir@nyu.edu>
2016-02-28 22:38:46 -0500

    added models

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(54))
     email = db.Column(db.String(120), unique=True)
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
 
     def __init__(self, username, password, email, firstName, lastName):
         self.username = username
@@ -25,9 +26,37 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+tags = db.Table('tags',
+        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+        db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
+)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     title = db.Column(db.String(255))
     description = db.Column(db.Text)
     date_posted = db.Column(db.DateTime, default = datetime.now())
-    status = db.Column(db.Enum('success','pending','failed'))
+    status = db.Column(db.Enum('success','pending','failed'), default='pending')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return '<Tag %r>' % self.name
+    
+
